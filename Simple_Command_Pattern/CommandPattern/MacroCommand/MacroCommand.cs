38056621482f6ae010613164928e4c4f40680212
Morten Filt <201600311@post.au.dk>
2019-10-20 22:31:46 +0200

    Added Macro Commands & .gitignor should now be working

---
+++ Simple_Command_Pattern/CommandPattern/MacroCommand/MacroCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace CommandPattern.CommandMacros
+{
+    public class MacroCommand : ICommand
+    {
+        private ICommand[] _commands;
+
+        public MacroCommand(ICommand[] commandCollection)
+        {
+            _commands = commandCollection;
+        }
+
+        public void Execute()
+        {
+            foreach (ICommand command in _commands)
+            {
+                command.Execute();
+            }
+        }
+    }
+}
--- Simple_Command_Pattern/CommandPattern/RemoteLoaderProgram.cs
+++ Simple_Command_Pattern/CommandPattern/RemoteLoaderProgram.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CommandPattern.CommandMacros;
 using CommandPattern.Commands;
 
 namespace CommandPattern
@@ -7,6 +9,7 @@ namespace CommandPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1 ON | 1 OFF Remote example");
             RemoteControl remoteControl = new RemoteControl(1);
 
             // Creating objects to control
@@ -18,6 +21,36 @@ namespace CommandPattern
 
             // Loading commands into the remoteControl
             remoteControl.SetCommand(0, garageLightOnCommand, garageLightOffCommand);
+
+            // Simulating button clicks
+            remoteControl.OnButtonPushed(0);
+            remoteControl.OffButtonPushed(0);
+
+            /*
+             * MACRO COMMAND EXAMPLE
+             */
+            Console.WriteLine("1 ON | 1 OFF Remote macro example");
+
+            // Creating objects to control
+            ICommand[] onCommandCollection = new ICommand[5];
+            ICommand[] offCommandCollection = new ICommand[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Light light = new Light("Light #" + (i+1));
+                onCommandCollection[i] = new LightOnCommand(light);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Light light = new Light("Light #" + (i+1));
+                offCommandCollection[i] = new LightOffCommand(light);
+            }
+
+            // Inserting command collection into a Macro Command
+            MacroCommand lightsOnMacro = new MacroCommand(onCommandCollection);
+            MacroCommand lightsOffMacro = new MacroCommand(offCommandCollection);
+
+            // Loading macro commands into the remoteControl
+            remoteControl.SetCommand(0, lightsOnMacro, lightsOffMacro);
 
             // Simulating button clicks
             remoteControl.OnButtonPushed(0);
